b5bed219fc176ea04a3d975f2acded642bc6570a
imosunov <imosunov@nsg.net.ru>
2020-02-03 22:35:56 +0300

    randon_string for client_id

--- config/releases.exs
+++ config/releases.exs
@@ -5,5 +5,3 @@ config :ileamo, IleamoWeb.Endpoint,
   # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
-
-config :ileamo, Ileamo.MQTT, client_id: "ileamo-gigalixir"
